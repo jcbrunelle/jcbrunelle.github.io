345e5d3d001a7b32d994cfa767ac4982c0ad6951
Jean-Charles Brunelle <jeancharles.brunelle@gmail.com>
2025-06-14 11:15:48 -1000

    modification du POC Comm2Dir

--- miniverse/poc/poccomm2d/main_scene.gd
+++ miniverse/poc/poccomm2d/main_scene.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
+var console
+var window
+func _ready():
+	console = JavaScriptBridge.get_interface("console")
+	window = JavaScriptBridge.get_interface("window")
+	console.log("ready")
+	
 func _on_button_pressed() -> void:
-	var window = JavaScriptBridge.get_interface("console")
-	if window:
-		window.log("Hello from Godot")
-	else:
-		print("pas de window")
+	var message = "Bonjour depuis Godot !"
+	#var js_code = "window.parent.postMessage(" + message + ", '*');"
+	#JavaScriptBridge.eval(js_code)
+	window.test(message);
