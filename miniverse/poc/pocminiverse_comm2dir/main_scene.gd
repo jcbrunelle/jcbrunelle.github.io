0500f29698f4658c9acf5b95169bee47c2678dc2
Jean-Charles Brunelle <jeancharles.brunelle@gmail.com>
2025-06-13 19:15:47 -1000

    modification du POC Comm2Dir

---
+++ miniverse/poc/pocminiverse_comm2dir/main_scene.gd
@@ -0,0 +1,29 @@
+extends Node
+
+var _js_bridge_refs = []
+
+@onready var username_label := $VBoxContainer/UsernameLabel
+
+func _ready():
+	if OS.has_feature("web"):
+		var window = JavaScriptBridge.get_interface("window")
+		var cb = JavaScriptBridge.create_callback(_on_message_from_react_native)
+		_js_bridge_refs.append(cb)
+		window.onMessageFromRN = cb
+
+func _on_message_from_react_native(args):
+	var json_str = args[0]
+	var result = JSON.parse_string(json_str)
+	if result:
+		match result["action"]:
+			"SET_USERNAME":
+				var username = result["payload"].get("username", "Anonyme")
+				username_label.text = "Utilisateur : " + username
+
+func _on_score_button_pressed():
+	send_to_native("SEND_SCORE", {"score": 12345})
+
+func send_to_native(action: String, payload: Dictionary):
+	var msg = {"action": action, "payload": payload}
+	var js_code = "window.ReactNativeWebView.postMessage(" + JSON.stringify(msg) + ");"
+	JavaScriptBridge.eval(js_code)
