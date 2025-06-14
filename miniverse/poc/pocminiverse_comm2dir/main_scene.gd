c39171164860af7d49cb109f59a2b2d11e02ac62
Jean-Charles Brunelle <jeancharles.brunelle@gmail.com>
2025-06-14 07:44:02 -1000

    modification du POC Comm2Dir

--- miniverse/poc/pocminiverse_comm2dir/main_scene.gd
+++ miniverse/poc/pocminiverse_comm2dir/main_scene.gd
@@ -1,15 +1,8 @@
 extends Node2D
 
-func _ready():
-	# Crée un bouton pour tester l'envoi
-	var button = Button.new()
-	button.text = "Envoyer message"
-	button.size = Vector2(200, 50)
-	button.position = Vector2(100, 100)
-	button.pressed.connect(_send_message_to_rn)
-	add_child(button)
-
 func _send_message_to_rn():
 	var window = JavaScriptBridge.get_interface("console")
 	if window:
 		window.log("Hello from Godot")
+	else:
+		print("pas de window")
