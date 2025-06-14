47876300504d5a3ed79a0407903175a60663f66c
Jean-Charles Brunelle <jeancharles.brunelle@gmail.com>
2025-06-14 12:19:59 -1000

    modification du POC Comm2Dir

--- miniverse/poc/poccomm2d/main_scene.gd
+++ miniverse/poc/poccomm2d/main_scene.gd
@@ -2,13 +2,27 @@ extends Node2D
 
 var console
 var window
+
+var _js_bridge_refs = []
+@onready var username_label := $UsernameLabel
+
 func _ready():
 	console = JavaScriptBridge.get_interface("console")
 	window = JavaScriptBridge.get_interface("window")
-	console.log("ready")
+	
+	var cb = JavaScriptBridge.create_callback(_on_message_from_react_native)
+	_js_bridge_refs.append(cb)
+	window.onMessageFromRN = cb
 	
 func _on_button_pressed() -> void:
 	var message = "Bonjour depuis Godot !"
-	#var js_code = "window.parent.postMessage(" + message + ", '*');"
-	#JavaScriptBridge.eval(js_code)
 	window.test(message);
+
+func _on_message_from_react_native(args):
+	var json_str = args[0]
+	var result = JSON.parse_string(json_str)
+	if result:
+		match result["action"]:
+			"SET_USERNAME":
+				var username = result["payload"].get("username", "Anonyme")
+				username_label.text = username
