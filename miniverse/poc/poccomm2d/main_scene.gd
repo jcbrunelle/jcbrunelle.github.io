e5012fb24d311b65fed8dcc34dca94404e77bbcb
Jean-Charles Brunelle <jeancharles.brunelle@gmail.com>
2025-06-14 15:56:31 -1000

    modification du POC Comm2Dir

--- miniverse/poc/poccomm2d/main_scene.gd
+++ miniverse/poc/poccomm2d/main_scene.gd
@@ -13,11 +13,15 @@ func _ready():
 	var cb = JavaScriptBridge.create_callback(_on_message_from_react_native)
 	_js_bridge_refs.append(cb)
 	window.onMessageFromRN = cb
-	
-func _on_button_pressed() -> void:
-	var message = JSON.stringify({"action":"GAME_OVER", "payload": {"score":23}})	
-	window.test(message);
+	send_message_to_react_native("READY",{})
 
+func _on_button_pressed() -> void:
+	send_message_to_react_native("GAME_OVER",{"score":23})
+
+func send_message_to_react_native(action, payload) -> void:
+	var message = JSON.stringify({"action":action,"payload":payload})
+	window.test(message);
+	
 func _on_message_from_react_native(args):
 	var json_str = args[0]
 	var result = JSON.parse_string(json_str)
