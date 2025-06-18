545ac4f58e5b85d0fbcff64261553e9690657ea2
Jean-Charles Brunelle <jeancharles.brunelle@gmail.com>
2025-06-17 23:42:26 -1000

    Modif librairie API

--- miniverse/api/miniverseapigdscript/test_scene.gd
+++ miniverse/api/miniverseapigdscript/test_scene.gd
@@ -3,11 +3,12 @@ extends Node2D
 
 var miniverseAPIScript = preload("res://addons/miniverse_api_gdscript/MiniverseAPIGDScript.gd")
 @onready var miniverse_api_instance = miniverseAPIScript.new()
+@onready var username_label := $UsernameLabel
 
 func _ready():
 	add_child(miniverse_api_instance)
-	print(miniverse_api_instance.initial_data)
+	username_label.text = miniverse_api_instance.initial_data['username']
 
 
 func _on_user_button_pressed() -> void:
-	print(miniverse_api_instance.get_initial_data())
+	username_label.text = miniverse_api_instance.initial_data['username']
